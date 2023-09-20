654592dc2031f5974ebe8a90491ee2817b48c474
IsabelleRancan <isabellerancan@gmail.com>
2023-09-19 20:08:56 -0400

    estudando

--- Promocao/script.js
+++ Promocao/script.js
@@ -1,4 +1,4 @@
-const dados = []
+let dados = [] //não pode ser const pq a gente vai mudar o que tem dentro
 
 const botao = document.getElementById('cadastrar')
 botao.addEventListener('click', cadastro)
@@ -14,7 +14,29 @@ function cadastro(){
     }
 
     const infos = {
-        nome = nome.value,
-        promocao = promocao.value
+        nome: nome.value,
+        promocao: promocao.value
     }
+
+    nome.value = ""
+    promocao.value = ""
+    
+    dados = getDados() //dados vai receber o que a função vale
+
+    dados.push(infos) //pegando dados e enfiando o que eu tenho de info dentro dele
+
+    setDados(dados) //enfiando dados na função set  
+    
+    console.log(dados)
+    
+}
+
+function getDados(){
+    return ( //a função vale o valor de dados ou array vazio
+        JSON.parse(localStorage.getItem("dados")) || []
+    )
+}
+
+function setDados(dados){
+        localStorage.setItem("dados", JSON.stringify(dados))
 }
